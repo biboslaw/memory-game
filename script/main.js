78e52e962c2fc872825b3e451b1d3c1d8137aa75
seba <sebastian.pieprzyk@gmail.com>
2018-04-03 23:42:24 +0200

    v0.7

--- script/main.js
+++ script/main.js
@@ -1,3 +1,28 @@
+var fontAwesomeArr = [
+    "far fa-bell",
+    "fas fa-bath",
+    "fas fa-beer",
+    "fas fa-birthday-cake",
+    "fas fa-bus",
+    "fas fa-chess-rook",
+    "fas fa-couch",
+    "fas fa-dove",
+    "fas fa-fighter-jet",
+    "fas fa-gift",
+    "far fa-hand-point-right",
+    "fab fa-hotjar",
+    "fab fa-itunes-note",
+    "fas fa-heart",
+    "fas fa-leaf",
+    "far fa-lightbulb",
+    "fas fa-motorcycle",
+    "fas fa-paw",
+    "fas fa-puzzle-piece",
+    "fab fa-reddit-alien"
+];
+var compare = false;
+var clickedCard;
+
 document.addEventListener("DOMContentLoaded", main());
 
 function main() {
@@ -24,11 +49,11 @@ function checkStorage(level) {
         'Joey: ': 150
     }
     var rankTable = {};
-    if (localStorage.length !== 0 && level == 'normal' && localStorage.getItem('normal')) {
-        rankTable = localStorage.getItem('normal');
+    if (localStorage.length !== 0 && level == 'normal' && localStorage.getItem(level)) {
+        rankTable = localStorage.getItem(level);
         return JSON.parse(rankTable);
-    } else if (localStorage.length !== 0 && level == 'hard' && localStorage.getItem('hard')) {
-        rankTable = localStorage.getItem('hard');
+    } else if (localStorage.length !== 0 && level == 'hard' && localStorage.getItem(level)) {
+        rankTable = localStorage.getItem(level);
         return JSON.parse(rankTable);
     } else if (localStorage.length == 0 && level == 'normal') {
         localStorage.setItem('hard', JSON.stringify(fakeRankTableHard));
@@ -52,49 +77,61 @@ function startGame() {
         }
     }
     cards = cards[level].cards;
-    var fontAwesomeArr = [
-        "far fa-bell",
-        "fas fa-bath",
-        "fas fa-beer",
-        "fas fa-birthday-cake",
-        "fas fa-bus",
-        "fas fa-chess-rook",
-        "fas fa-couch",
-        "fas fa-dove",
-        "fas fa-fighter-jet",
-        "fas fa-gift",
-        "far fa-hand-point-right",
-        "fab fa-hotjar",
-        "fab fa-itunes-note",
-        "fas fa-heart",
-        "fas fa-leaf",
-        "far fa-lightbulb",
-        "fas fa-motorcycle",
-        "fas fa-paw",
-        "fas fa-puzzle-piece",
-        "fab fa-reddit-alien"
-    ];
     var cardsArray = [];
     shuffleArray(fontAwesomeArr);
-    for (var i = 0; i < cards / 2; i++) {
-        cardsArray.push(fontAwesomeArr[i]);
-        cardsArray.push(fontAwesomeArr[i]);
-    };
-    var compare = "";
+    cardsArray = fontAwesomeArr.slice(1, cards / 2 + 1).concat(fontAwesomeArr.slice(1, cards / 2 + 1));
     var modal = document.querySelector("#modal");
     modal.classList.add("hidden2");
     shuffleArray(cardsArray);
     for (i = 0; i < cardsArray.length; i++) {
-        var mainCard = createCards(cardsArray[i], mainCard, i);
-        mainCard.addEventListener("click", function (e) {
-            compare = compareCards(e, compare, ranking);
-        });
+        var mainCard = createCards(cardsArray[i], mainCard, i, ranking);
         gameBoard.appendChild(mainCard);
     }
     gameBoard.parentElement.classList.remove("hidden");
 }
 
-function createCards(arrayObj, mainCard, i) {
+function compareCards(e, ranking) {
+    var clickedCards = document.querySelectorAll(".clicked");
+    if (clickedCards.length == 0 || e.target.parentElement.getAttribute("id") == clickedCards[0].parentElement.getAttribute("id")) {
+        e.target.classList.add("clicked");
+        return;
+    } else if (clickedCards.length == 1) {
+        e.target.classList.add("clicked");
+        compare = clickedCards[0].parentElement.querySelector('.background').innerHTML == e.target.parentElement.querySelector('.background').innerHTML;
+        setTimeout(hideCards, 500, compare, ranking);
+    }
+    return;
+}
+
+function hideCards(compare, ranking) {
+    var cardsToHide = document.querySelectorAll(".clicked");
+    var scoreDiv = document.querySelector("#score span");
+    var score = Number(scoreDiv.innerHTML);
+    if (compare) {
+        for (var i = 0; i < cardsToHide.length; i++) {
+            cardsToHide[i].classList.remove("clicked");
+            cardsToHide[i].parentElement.removeAttribute("id");
+            cardsToHide[i].classList.add("hidden2");
+            cardsToHide[i].parentElement.querySelector(".background").classList.add("hidden2");
+        }
+        scoreDiv.innerHTML = score + 1;
+        ifEnd(ranking);
+    } else {
+        for (var i = 0; i < cardsToHide.length; i++) {
+            cardsToHide[i].classList.remove("clicked");
+        }
+        scoreDiv.innerHTML = score + 1;
+    }
+}
+
+function checkClicked() {
+    var clicked = document.querySelectorAll(".clicked")
+    if (clicked.length == 2) {
+        return true;
+    } else return false;
+}
+
+function createCards(faIconClass, mainCard, i, ranking) {
     mainCard = document.createElement('div');
     var foreground = document.createElement('div');
     var background = document.createElement('div');
@@ -103,10 +140,14 @@ function createCards(arrayObj, mainCard, i) {
     mainCard.setAttribute('id', i);
     foreground.classList.add('foreground');
     background.classList.add('background');
-    faIcon.setAttribute("class", arrayObj);
+    faIcon.setAttribute("class", faIconClass);
     background.appendChild(faIcon);
     mainCard.appendChild(background)
     mainCard.appendChild(foreground);
+    foreground.addEventListener("click", function (e) {
+        e.stopPropagation();
+        compareCards(e, ranking);
+    });
     return mainCard;
 }
 
@@ -119,39 +160,6 @@ function shuffleArray(array) {
     }
 }
 
-function compareCards(e, compare, ranking) {
-    if (checkClicked()) {
-        return "";
-    }
-    var temp = e.target.parentElement.querySelector('.background')
-    if (compare == "") {
-        e.target.classList.add('clicked');
-        return compare = e.target.parentElement.querySelector('.background');
-    } else if (compare.parentElement.getAttribute('id') == e.target.parentElement.getAttribute('id')) {
-        return compare;
-    } else if (compare.innerHTML == temp.innerHTML) {
-        e.target.classList.add('clicked');
-        setTimeout(hideCards, 300, compare, e, ranking);
-        return compare = "";
-    }
-    e.target.classList.add('clicked');
-    setTimeout(resetCards, 700, compare, e);
-    return compare = "";
-}
-
-function hideCards(compare, e, ranking) {
-    compare.parentElement.classList.add('hidden');
-    compare.parentElement.removeAttribute('id');
-    e.target.parentElement.classList.add('hidden');
-    e.target.parentElement.removeAttribute('id');
-    compare.nextSibling.classList.remove("clicked");
-    e.target.classList.remove("clicked");
-    var scoreDiv = document.querySelector("#score span");
-    var score = Number(scoreDiv.innerHTML);
-    scoreDiv.innerHTML = score + 1;
-    ifEnd(ranking);
-}
-
 function resetCards(compare, e) {
     e.target.classList.remove('clicked');
     compare.parentElement.querySelector(".foreground").classList.remove('clicked');
@@ -160,14 +168,8 @@ function resetCards(compare, e) {
     scoreDiv.innerHTML = score + 1;
 }
 
-function checkClicked() {
-    var clicked = document.querySelectorAll(".clicked")
-    if (clicked.length == 2) {
-        return true;
-    } else return false;
-}
-
 function ifEnd(ranking) {
+    console.log("weszlo")
     var board = document.querySelectorAll('#gameBoard > div');
     var count = 0;
     for (i = 0; i < board.length; i++) {
@@ -179,22 +181,17 @@ function ifEnd(ranking) {
         var score = document.querySelector("#score span").innerHTML;
         var modal = document.querySelector("#modal");
         var applyBtn = document.querySelector(".applyBtn");
-        var input = document.getElementById("winner");
+        var score2 = document.querySelector(".score2");
         modal.querySelector("#sectionPlay").classList.add("hidden2");
         modal.querySelector("#sectionEnterName").classList.remove("hidden2");
         score = Number(score);
+        score2.innerHTML = "Score: " + score;
         modal.classList.remove("hidden2");
         document.querySelector("#game").classList.add("hidden")
         modal.querySelector(".rank").classList.remove("hidden2");
         applyBtn.addEventListener("click", function (e) {
             e.target.parentElement.classList.add("hidden2");
             gameEnd(ranking, score, e, modal);
-        });
-        input.addEventListener("keyup", function (event) {
-            event.preventDefault();
-            if (event.keyCode === 13) {
-                document.getElementById("applyBtn").click();
-            }
         });
     }
 }
@@ -214,7 +211,7 @@ function gameEnd(ranking, score, e, modal) {
     ranking.sort(function (a, b) {
         return a[1] - b[1];
     });
-    createTable(ranking, rank);
+    createTable(ranking, rank, name);
     score = document.querySelector("#score span");
     score.innerHTML = 0;
     var finalRanking = {};
@@ -224,7 +221,7 @@ function gameEnd(ranking, score, e, modal) {
     localStorage.setItem(finalRanking.level, JSON.stringify(finalRanking));
 }
 
-function createTable(ranking, rank) {
+function createTable(ranking, rank, name) {
     var table = document.createElement("table");
     var th = document.createElement("thead");
     var thName = document.createElement("th");
@@ -241,6 +238,9 @@ function createTable(ranking, rank) {
     table.classList.add("rankList");
     for (var i = 1; i < ranking.length; i++) {
         var newRow = table.insertRow(-1);
+        if (ranking[i] == name) {
+            newRow.setAttribute("class", "highlighted")
+        }
         var nameCell = newRow.insertCell(0);
         var scoreCell = newRow.insertCell(1);
         nameCell.innerHTML = ranking[i][0];
